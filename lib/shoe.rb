cf6570c8040ea179ce1fdf78ac1902b523b824a3
valeriacruise <valeriacruise22@gmail.com>
2018-07-13 15:11:58 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -12,13 +12,7 @@ class Shoe
 
   def cobble
     puts 'Your shoe is as good as new!'
-  end
-    
-  def condition
-    @condition= "new"
+    @condition= new
   end
   
-   def condition
-    @condition= "tattered"
-  end
 end
