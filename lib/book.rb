7f5f9cb6da193105fa5293cb1dce49013fe5c0e1
valeriacruise <valeriacruise22@gmail.com>
2018-07-13 14:12:18 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
 # book.rb
+class Book 
+  attr_accessor :author, :page_count, :genre
+  
+  def initialize(title)
+    @title= title
+  end
+  
+  def title
+    @title
+  end
+  
+  def page_count= (num)
+    @page_count= (num)
+  end 
+  
+  def genre= (genre)
+    @genre= genre
+  end 
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
+
 # Add you Book class here
