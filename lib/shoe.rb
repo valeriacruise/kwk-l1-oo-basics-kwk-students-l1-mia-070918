beaec8251905fe7c717636adf2897a4015d9174c
valeriacruise <valeriacruise22@gmail.com>
2018-07-13 14:23:51 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,19 @@
 
 class Shoe
-  attr_accessor :brand, :color
+  def initialize(brand)
+    @brand= brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+  def initialize(color)
+    @brand= brand
+  end
+  
+  def color
+    @color
+  end
   
 end
